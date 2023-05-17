2c3066b7ccb194adc2ecf3286f395aaa68858bce
Navil <navyadenip@gmail.com>
2023-05-16 21:52:16 -0600

    Commit

--- Drink/front-end/scripts/stock.js
+++ Drink/front-end/scripts/stock.js
@@ -1,29 +1,32 @@
-//Recuperar el boton para añadir eventos
+//Recuperar el boton para añadir ingrediente
 const addEventBtn = document.querySelector(".new-ingredient");
 
-//Recuperar la seccion para añadir eventos
+//Recuperar la seccion para añadir ingrediente
 const addIngredContainer = document.querySelector(".add-new-ingredient");
 
 //Recuperar el boton para salir
 const closeBtn = document.querySelector(".close");
 
-//Recuperar el nombre del solicitante
+//Recuperar el nombre del ingrediente
 const addIngredientName = document.querySelector(".ingredient-name");
 
-//Recuperar la clase o carrera
+//Recuperar la categoria
 const addIngredientCategory = document.querySelector(".ingredient-category");
 
-
-//Recuperar la clase o carrera
+//Recuperar el precio
 const addIngredientPrice= document.querySelector(".ingredient-price");
 
-// Recuperar el boton para hacer una nueva reservacion
+// Recuperar el boton para guardar los cambios
 const addEventSubmit = document.querySelector(".add-event-btn");
+
+// Recuperar el fondo negro
+const shape = document.querySelector('.shape');
 
 //Agregar funcionalidad para abrir y cerrar la ventana
 //Abrir la ventana
 function open() {
     addIngredContainer.classList.toggle("active-n");
+    shape.classList.remove('hide');
 }
 
 addEventBtn.addEventListener("click", open);
@@ -46,6 +49,7 @@ function close() {
     }
     addEventSubmit.classList.remove("send-error");
     addIngredContainer.classList.remove("active-n");
+    shape.classList.add('hide');
 }
 
 closeBtn.addEventListener("click", close);
